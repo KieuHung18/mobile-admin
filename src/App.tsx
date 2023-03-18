6d8c78d63f8654c8e5bd8088e4adab4bd5bf31ba
hung <hung@Acer>
2023-03-18 11:53:30 +0700

    button

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,25 @@ import React from "react";
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ComponentsDemo from "./features/components-demo";
+import Home from "./features/home";
+import NotFound from "./features/not-found";
 
 function App() {
-  const testTailwind = (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Simple React Typescript Tailwind Sample
-    </h1>
-  );
   const router = createBrowserRouter([
     {
       path: "/",
-      element: testTailwind,
+      element: <Home />,
+    },
+    {
+      path: "/*",
+      element: <NotFound />,
     },
     {
       path: "/components-demo",
       element: <ComponentsDemo />,
     },
   ]);
-  return (
-    <RouterProvider router={router} />
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      primary: {
+        0: "#191919",
+        5: "#202020",
+        10: "#2a2a2a",
+        15: "#666666",
+        20: "#ddd",
+      },
+      neutral: {
+        dark: "#000000",
+        light: "#fff",
+      },
+    },
+    fontFamily: {
+      muli: ["Muli", "sans-serif"],
+      futura: ["Futura", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
